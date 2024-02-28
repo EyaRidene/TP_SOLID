76fa2cb99e7efb3a870805d942af50ae93bc7e15
eya ridene <eya.ridene@insat.u-carthage.tn>
2024-02-28 22:46:19 +0100

    ISP

--- ISP/src/com/directi/training/isp/exercise/TimedDoor.java
+++ ISP/src/com/directi/training/isp/exercise/TimedDoor.java
@@ -2,7 +2,7 @@ package com.directi.training.isp.exercise;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class TimedDoor implements Door
+public class TimedDoor implements Door, ITimedCallback
 {
     private static final int TIME_OUT = 100;
     private boolean _locked;
@@ -43,11 +43,5 @@ public class TimedDoor implements Door
     public void timeOutCallback()
     {
         _locked = true;
-    }
-
-    @Override
-    public void proximityCallback()
-    {
-        throw new NotImplementedException();
     }
 }
